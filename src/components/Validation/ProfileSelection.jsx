251d6ec033efb7afa55c03b16f05624c8e52e042
muqsitadam <muqsitadam@gmail.com>
2024-11-04 13:21:13 +0100

    Worked on the ui of the profile selection

--- src/components/Validation/ProfileSelection.jsx
+++ src/components/Validation/ProfileSelection.jsx
@@ -4,6 +4,7 @@ import NavBar from '../LandingPage/NavBar';
 import Footer from '../LandingPage/Footer';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import bgImage from '../../assets/login-bg.png'
 
 const ProfileSelection = ({ onNext }) => {
   const [selectedProfiles, setSelectedProfiles] = useState([]);
@@ -62,30 +63,36 @@ const ProfileSelection = ({ onNext }) => {
   return (
     <div>
       <NavBar bgColor="bg-farmersmartDarkGreen" />
-      <div className='my-32'>
-        <div className="p-6 bg-white shadow-lg rounded-lg max-w-md mx-auto mt-12">
-          <h2 className="text-2xl font-semibold text-center mb-6">Select Profiles</h2>
-          {error && <p className="text-red-500 text-center">{error}</p>}
-          <div className="space-y-4">
-            {['Farmer', 'Buyer', 'Logistics', 'Partner'].map((profile) => (
-              <div key={profile} className="flex items-center">
-                <Checkbox
-                  checked={selectedProfiles.includes(profile)}
-                  onChange={() => handleProfileChange(profile)}
-                  color="primary"
-                />
-                <label className="text-lg">{profile}</label>
-              </div>
-            ))}
+      <div className="relative min-h-screen flex items-center justify-center">
+        <div className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+          style={{
+          backgroundImage: `url(${bgImage})`
+        }}>
+        </div>
+        <div className="z-10 mx-auto w-1/2">
+          <div className="bg-farmersmartPaleGreen w-full shadow-lg p-8 rounded-3xl max-w-md mx-auto mt-12">
+            <h2 className="text-2xl font-semibold text-center mb-6">Select Profiles</h2>
+            {error && <p className="text-red-500 text-center">{error}</p>}
+            <div className="space-y-4">
+              {['Farmer', 'Buyer', 'Logistics', 'Partner'].map((profile) => (
+                <div key={profile} className="flex items-center">
+                  <Checkbox
+                    checked={selectedProfiles.includes(profile)}
+                    onChange={() => handleProfileChange(profile)}
+                    color="primary"
+                  />
+                  <label className="text-lg">{profile}</label>
+                </div>
+              ))}
+            </div>
+            <button
+              type='submit'
+              className="bg-farmersmartDarkGreen my-4 text-center text-white text-xl font-semibold py-3 px-10 rounded-full transition duration-300"
+              onClick={handleSubmit}
+            >
+              Join Us
+            </button>
           </div>
-          <Button
-            variant="contained"
-            color="primary"
-            className="w-full mt-6"
-            onClick={handleSubmit}
-          >
-            Join Us
-          </Button>
         </div>
       </div>
       <Footer className='mt-auto w-full' />
