6b480a3af9fbe161d1e367c825fbf0467b0d5260
muqsitadam <muqsitadam@gmail.com>
2025-02-22 11:56:25 +0100

    Delivery route endpoint corrections

--- src/components/Marketplace/DeliveryRoute.jsx
+++ src/components/Marketplace/DeliveryRoute.jsx
@@ -56,7 +56,7 @@ const DeliveryRoute = () => {
 
   const handleSubmit = async () => {
     const deliveryForm = { ...formData };
-         const url =`${import.meta.env.VITE_API_BASE_URL}/rdeli_move`
+         const url =`${import.meta.env.VITE_API_BASE_URL}/rdeli`
   
     try {
       console.log(deliveryForm)
